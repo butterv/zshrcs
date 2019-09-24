4a9593a595d22bb3906c760d90d839be2f23bd81
istsh <github3104xniiapm@gmail.com>
2019-09-25 00:37:42 +0900

    Update go.zsh

--- go.zsh
+++ go.zsh
@@ -4,3 +4,13 @@ eval "$(anyenv init -)"
 export GOPATH="$HOME/go"
 export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
 export GO111MODULE=on
+
+alias anyenv-v="anyenv versions"
+alias anyenv-u="anyenv update"
+alias anyenv-gitpu="anyenv git pull"
+alias anyenv-l="anyenv install -l"
+
+alias goenv-l="goenv install -l"
+alias goenv-i="goenv install"
+alias goenv-gl="goenv global"
+alias goenv-lo="goenv local"
