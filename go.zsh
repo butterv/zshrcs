68ecd874b2763917785de99575d33d324c935c90
istsh <github3104xniiapm@gmail.com>
2019-10-15 00:22:38 +0900

    Add protoc function

--- go.zsh
+++ go.zsh
@@ -14,3 +14,12 @@ alias goenv-l="goenv install -l"
 alias goenv-i="goenv install"
 alias goenv-gl="goenv global"
 alias goenv-lo="goenv local"
+
+function pb-gen() {
+	# pb-gen ./app/proto/v1 user.proto
+	protoc -I$1 -I$GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.11.3/third_party/googleapis --go_out=plugins=grpc:$1/pb $2
+}
+function pb-gw-gen() {
+	# pb-gw-gen ./app/proto/v1 user.proto
+	protoc -I$1 -I$GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.11.3/third_party/googleapis --grpc-gateway_out=logtostderr=true:$1/pb $2
+}
