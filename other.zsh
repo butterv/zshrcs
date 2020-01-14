e87dcc0d23deb3beb1c2e6535f46bd01ec26a06d
istsh <github3104xniiapm@gmail.com>
2020-01-15 01:20:07 +0900

    Update other.zsh

--- other.zsh
+++ other.zsh
@@ -9,9 +9,11 @@ alias mkdir='mkdir -p'
 alias v='vim'
 alias shreboot='exec $SHELL -l'
 
+export PATH=/usr/local/bin:$PATH
 export PATH=$HOME/.nodebrew/current/bin:$PATH
 export PATH="$HOME/.yarn/bin:$PATH"
 export PATH=$PATH:$HOME/google-cloud-sdk/bin
+export PATH="$PATH:$HOME/development/flutter/bin"
 
 alias docker-stop-all='docker stop $(docker ps -q)'
 
@@ -23,7 +25,6 @@ export EDITOR="vim"
 # eval "$(direnv hook bash)"
 # source /usr/local/etc/bash_completion.d/git-prompt.sh
 # source /usr/local/etc/bash_completion.d/git-completion.bash
-# export PATH="$PATH:$HOME/development/flutter/bin"
 
 echo -ne "\033]0;$(pwd | rev | awk -F \/ '{print "/"$1"/"$2}'| rev)\007"
 function chpwd() {
