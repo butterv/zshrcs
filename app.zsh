1cbd77ef1b93b8e0b2eb76dea6e037113a5911e9
butterv <github3104xniiapm@gmail.com>
2021-01-26 21:57:43 +0900

    Remove all zsh files

--- app.zsh
+++
@@ -1,6 +0,0 @@
-# alias goland='/usr/local/bin/goland'
-
-alias sublime='open -a /Applications/Sublime\ Text.app'
-alias tableplus='open -a /Applications/TablePlus.app'
-alias sequelpro='open -a /Applications/Sequel\ Pro.app'
-alias keeper='open -a /Applications/Keeper\ Password\ Manager.app'
